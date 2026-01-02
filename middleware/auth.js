26925cefeefc3c63f37b07bd9b7635444529eb27
Tomasz Libich <t.libich@gmail.com>
2026-01-02 14:01:05 +0000

    Add request to verify incoming token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,32 @@
-export const auth = (req, res) => {
+import crypto from "node:crypto";
+
+export const auth = async (req, res, next) => {
+  const requestCorrelationId = crypto.randomUUID();
+
   try {
     const token = req.headers.authorization?.startsWith("Bearer ")
       ? req.headers.authorization.slice(7)
       : req.cookies?.accessToken;
 
-    const payload = verifyAccessToken(token, {
-      secret: process.env.JWT_ACCESS_SECRET,
+    const response = await fetch(process.env.VERIFY_ACCESS_TOKEN_URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Request-Id": requestCorrelationId,
+        "Content-Type": "application/json",
+        "Content-Length": "0",
+      },
     });
 
-    req.user = payload;
+    if (response.status !== 200) {
+      throw new Error("Unauthorized");
+    }
 
     next();
   } catch (e) {
+    console.error("Auth error:", e);
+
     res.status(401).json({ message: "Unauthorized" });
   }
 };
---
+++ scripts/restart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop lbt-gateway
+docker compose up -d --build
